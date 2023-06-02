5ecc261535d036f10222ed0c6dd73aa0f772519b
Jon Leopard <contact@jonleopard.com>
2023-06-02 09:21:34 -0700

    fix: weird bug where on some browsers background is gray

--- src/components/TextIntro.tsx
+++ src/components/TextIntro.tsx
@@ -1,6 +1,6 @@
 export const TextIntro: React.FC = () => {
     return (
-        <div className="text-center">
+        <div className="text-center text-white">
             <h1 className="text-6xl">Bulding small ideas for the masses.</h1>
             <p className="pt-4 text-xl text-gray-400">
                 We&apos;re small, remote-based team with a passion for building
