941f26eb8bdbefdddfcad0a3431f32bf925c1983
Jon Leopard <contact@jonleopard.com>
2023-06-05 13:01:52 -0700

    fix: typo

--- src/components/TextIntro.tsx
+++ src/components/TextIntro.tsx
@@ -1,7 +1,7 @@
 export const TextIntro: React.FC = () => {
     return (
         <div className="text-center text-white">
-            <h1 className="text-6xl">Bulding small ideas for the masses.</h1>
+            <h1 className="text-6xl">Building small ideas for the masses.</h1>
             <p className="pt-4 text-xl text-gray-400">
                 We&apos;re small, remote-based team with a passion for building
                 software for the web.
