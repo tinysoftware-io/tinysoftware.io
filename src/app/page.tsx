be1def2299e3e9ed3381b2936bb5ba8b72c234de
Jon Leopard <contact@jonleopard.com>
2023-06-05 13:46:48 -0700

    feat: add GA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,41 @@
 import Image from 'next/image'
 import { TextIntro } from '@/components/TextIntro'
 import { Footer } from '@/components/Footer'
+import Script from 'next/script'
 
 const Home: React.FC = () => {
     return (
-        <div className="flex flex-col min-h-screen">
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8 pt-10">
-                <Image
-                    src="/tiny_software_logo.svg"
-                    alt="Tiny Softare Logo"
-                    width={250}
-                    height={42}
-                    priority
-                />
-            </div>
+        <>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-            <main className="my-auto sm:px-6 lg:px-8">
-                <TextIntro />
-            </main>
-            <Footer />
-        </div>
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+            </Script>
+            <div className="flex flex-col min-h-screen">
+                <div className="container mx-auto px-4 sm:px-6 lg:px-8 pt-10">
+                    <Image
+                        src="/tiny_software_logo.svg"
+                        alt="Tiny Softare Logo"
+                        width={250}
+                        height={42}
+                        priority
+                    />
+                </div>
+
+                <main className="my-auto sm:px-6 lg:px-8">
+                    <TextIntro />
+                </main>
+                <Footer />
+            </div>
+        </>
     )
 }
 
