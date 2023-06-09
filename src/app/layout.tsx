b342590aec1a0096ced7480348cc7e42baa9a4ac
Jon Leopard <contact@jonleopard.com>
2023-06-08 19:34:44 -0700

    fix: put ga in the right component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,35 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: 'Tiny Software',
-    description: 'Building small ideas for the masses',
-}
+  title: "Tiny Software",
+  description: "Building small ideas for the masses",
+};
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,28 @@
-import Image from 'next/image'
-import { TextIntro } from '@/components/TextIntro'
-import { Footer } from '@/components/Footer'
-import Script from 'next/script'
+import Image from "next/image";
+import { TextIntro } from "@/components/TextIntro";
+import { Footer } from "@/components/Footer";
 
 const Home: React.FC = () => {
-    return (
-        <>
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-                strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 pt-10">
+          <Image
+            src="/tiny_software_logo.svg"
+            alt="Tiny Softare Logo"
+            width={250}
+            height={42}
+            priority
+          />
+        </div>
 
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-        `}
-            </Script>
-            <div className="flex flex-col min-h-screen">
-                <div className="container mx-auto px-4 sm:px-6 lg:px-8 pt-10">
-                    <Image
-                        src="/tiny_software_logo.svg"
-                        alt="Tiny Softare Logo"
-                        width={250}
-                        height={42}
-                        priority
-                    />
-                </div>
+        <main className="my-auto sm:px-6 lg:px-8">
+          <TextIntro />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-                <main className="my-auto sm:px-6 lg:px-8">
-                    <TextIntro />
-                </main>
-                <Footer />
-            </div>
-        </>
-    )
-}
-
-export default Home
+export default Home;
